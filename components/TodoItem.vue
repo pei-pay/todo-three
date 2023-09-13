08df321610f3e2c679f340f73ed8dcd3556b994d
pei-pay <gleektennis@icloud.com>
2023-09-13 21:03:06 +0900

    tweak add semiColons

--- components/TodoItem.vue
+++ components/TodoItem.vue
@@ -2,15 +2,15 @@
 import { Todo } from '~/entity/todo';
 
 interface Props {
-  todo: Todo
+  todo: Todo;
 }
 interface Emits {
   (e: 'handleUpdateTitle', id: number, title: string): void,
-  (e: 'handleUpdateCompletedStatus', id: number, isCompleted: boolean): void
-  (e: 'handleDelete', id: number): void
+  (e: 'handleUpdateCompletedStatus', id: number, isCompleted: boolean): void;
+  (e: 'handleDelete', id: number): void;
 }
-const props = defineProps<Props>()
-const emit = defineEmits<Emits>()
+const props = defineProps<Props>();
+const emit = defineEmits<Emits>();
 
 const isUpdating = ref(false);
 const updatingTitle = ref(props.todo.title);
@@ -18,32 +18,20 @@ const updatingTitle = ref(props.todo.title);
 const handleUpdateSave = () => {
   emit('handleUpdateTitle', props.todo.id, updatingTitle.value);
   isUpdating.value = false;
-}
+};
 const handleUpdateReset = () => {
   updatingTitle.value = props.todo.title;
   isUpdating.value = false;
-}
+};
 </script>
 
 <template>
   <li class="todo-item">
-    <input 
-      type="checkbox"
-      :checked="todo.isCompleted"
-      @change="$emit('handleUpdateCompletedStatus', todo.id,  !todo.isCompleted)"
-    >
-    <input 
-      v-if="isUpdating"
-      v-model="updatingTitle"
-      type="text"
-      @keyup.enter="handleUpdateSave"
-      @blur="handleUpdateReset"
-    >
-    <span
-      v-else
-      :class="{ completed: todo.isCompleted }"
-      @click="isUpdating = true"
-    >
+    <input type="checkbox" :checked="todo.isCompleted"
+      @change="$emit('handleUpdateCompletedStatus', todo.id, !todo.isCompleted)">
+    <input v-if="isUpdating" v-model="updatingTitle" type="text" @keyup.enter="handleUpdateSave"
+      @blur="handleUpdateReset">
+    <span v-else :class="{ completed: todo.isCompleted }" @click="isUpdating = true">
       {{ todo.title }}
     </span>
     <button @click="$emit('handleDelete', todo.id)">Delete</button>
@@ -57,20 +45,25 @@ li.todo-item {
   justify-content: space-between;
   border: 1px solid #ccc;
   padding: 4px;
+
   input[type="checkbox"] {
     margin-right: 4px;
   }
+
   input[type="text"] {
     border: none;
     width: 100%;
     padding: 4px;
   }
+
   span {
     width: 100%;
+
     &.completed {
       text-decoration: line-through;
     }
   }
+
   button {
     margin-left: 4px;
     background-color: red;
